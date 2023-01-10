0b0906f9c2467a18bd5cc317f141af4f0c31c9a7
Paulo Ricardo <notadead5@gmail.com>
2023-01-09 23:57:36 -0300

    Listar pokemons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { getPokemons } from './api';
 import './App.css';
 import Navbar from './components/Navbar';
 import  Pokedex  from './components/Pokedex';
 import Searchbar from './components/Searchbar';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [page, setpage] = useState([]);
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const result = await getPokemons();
+      setPokemons(result);
+      setLoading(false);
+    } catch (error) {
+      console.log("fetchPokemons error: ", error)
+    }
+  }
+
+  useEffect(() => {
+    console.log("carregou")
+    fetchPokemons();
+  }, [pokemons])
+
   return (
 
     <div>
       <Navbar/>
-      <Searchbar
-        
-      />
-      <Pokedex/>
+      <Searchbar/>
+      <Pokedex pokemons={pokemons} loading={loading}/>
     </div>
   );
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Pokedex = () => {
+const Pokedex = (props) => {
+    const {pokemons, loading} = props;
     return(
         <div>
             <div className='pokedex-header'>
                 <h1>Pokedex</h1>
                 <div>Paginação</div>
             </div>
+            {loading ? (<div>Loading...</div>) : (<div className='pokedex-grid'>  </div>)}
         </div>
     )
 }
